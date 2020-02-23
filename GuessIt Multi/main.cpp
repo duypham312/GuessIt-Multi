2d6bdce31effce1409fc96eb8b08b9c4583f9af0
Duy <worthlessduy@gmail.com>
2020-02-23 11:40:17 +0700

    Initial Commit

---
+++ GuessIt Multi/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GuessIt Multi
+//
+//  Created by Duy on 2/23/20.
+//  Copyright © 2020 Duy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
